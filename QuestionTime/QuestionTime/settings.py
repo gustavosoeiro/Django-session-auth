25763a9c439a2a1ed6ea29f14d7d11a26952fb25
Gustavo Sooeiro <gustavosooeiro@gmail.com>
2020-12-18 13:43:22 -0300

    first commit

--- QuestionTime/QuestionTime/settings.py
+++ QuestionTime/QuestionTime/settings.py
@@ -92,10 +92,15 @@ WSGI_APPLICATION = 'QuestionTime.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ.get('CA_DB_NAME'),
+        'USER': os.environ.get('CA_DB_USER'),
+        'PASSWORD': os.environ.get('CA_DB_PW'),
+        'HOST': os.environ.get('CA_DB_H'),
+        'PORT': '5432',    
     }
 }
+
 
 
 # Password validation
